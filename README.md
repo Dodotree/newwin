781185c673ea99ba33b7a526107075e39b48b62c
Paul T <paul@pault.com>
2024-03-05 14:19:09 -0800

    Some preliminary commits to get the project started.

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # newwin README
 
-This is an extension that allows a new type of (programmable) windows in VScode.
+This is bunch of files for the extension that allows a new type of (programmable) windows in VScode.
 To use it - Right Click and pick "New Window" from the context menu.
 
-More documentation will be provided. Eventually.
+After I spend a few hours on vscode extensions (which is windows 3.0 basically) - this will be on hold for a little.
 
 Mar 05 2024
 
--- src/test/extension.ts
+++ src/test/extension.ts
@@ -2,13 +2,39 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+export function NewCustomWindow() {
+	// Create and show a new webview
+	const panel = vscode.window.createWebviewPanel(
+		'catCoding',
+		'Cat Coding',
+		vscode.ViewColumn.One,
+		{}
+	);
+	panel.webview.html = getWebviewContent();
+}
+
+export function getWebviewContent() {
+	return `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Cat Coding</title>
+	</head>
+	<body>
+		<img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+	</body>
+	</html>`;
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "newwin" is now active!');
+	console.log('Custom window with "newwin" is now active!');
+	vscode.window.showInformationMessage('Hello World from newwin!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
